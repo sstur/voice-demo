1c443b201dc5241bef93c3468cf1f4644e30f0d0
Simon <sstur@me.com>
2024-07-02 22:24:06 -0700

    Implement components/core

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,9 +1,9 @@
-import { Button, YStack } from 'tamagui';
+import { Button, VStack } from '../components/core';
 
 export default function App() {
   return (
-    <YStack flex={1} justifyContent="center" alignItems="center">
+    <VStack flex={1} justifyContent="center" alignItems="center">
       <Button>{t('Hello world!')}</Button>
-    </YStack>
+    </VStack>
   );
 }
---
+++ mobile/src/components/core/index.ts
@@ -0,0 +1,8 @@
+export * from 'tamagui';
+
+export {
+  // Sorted alphabetically
+  Paragraph as Text,
+  YStack as VStack,
+  YStack as HStack,
+} from 'tamagui';
