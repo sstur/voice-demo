99819b9dee334b88b394d09bb0a3c1fa01bc92a8
Simon <sstur@me.com>
2024-07-02 22:30:06 -0700

    Add @tamagui/lucide-icons

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,9 +1,11 @@
+import { Home } from '@tamagui/lucide-icons';
+
 import { Button, VStack } from '../components/core';
 
 export default function App() {
   return (
     <VStack flex={1} justifyContent="center" alignItems="center">
-      <Button>{t('Hello world!')}</Button>
+      <Button icon={<Home />}>{t('Hello world!')}</Button>
     </VStack>
   );
 }
