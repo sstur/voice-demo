793cb5dd90301d873c67aa905f6e44c9ed6bd0e3
Simon <sstur@me.com>
2024-07-02 22:02:13 -0700

    Add prettier-plugin-sort-imports

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,5 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
