a5970667ddff739902f12d88f1154692f2494951
Simon <sstur@me.com>
2024-07-30 21:46:36 -0500

    Fixup: Mobile: remove dead code from playSound

--- mobile/src/recording/playSound.ts
+++ mobile/src/recording/playSound.ts
@@ -31,46 +31,6 @@ export async function playSound(init: { uri: string; onDone: () => void }) {
   };
 }
 
-// export async function playSound(init: { uri: string; onDone: () => void }) {
-//   const { uri, onDone } = init;
-//   console.log('>> Playing sound:', { uri });
-//   await Audio.setAudioModeAsync({
-//     playsInSilentModeIOS: true,
-//   });
-//   // Load the sound from a remote URL
-//   const result = await safeInvoke(() =>
-//     Audio.Sound.createAsync({ uri }, undefined, null, false),
-//   );
-//   if (!result.ok) {
-//     console.log('>> E1:', result.error);
-//     throw result.error;
-//   }
-
-//   console.log('>> Checkpoint 1.');
-
-//   const { sound } = result.result;
-
-//   // Set a callback to run when the sound finishes playing
-//   sound.setOnPlaybackStatusUpdate((status) => {
-//     console.log('>> setOnPlaybackStatusUpdate:', status);
-//     if (!status.isLoaded || status.didJustFinish) {
-//       onDone();
-//       void sound.unloadAsync();
-//     }
-//   });
-
-//   console.log('>> Checkpoint 2.');
-
-//   // Play the sound
-//   const foo = await safeInvoke(() => sound.playAsync());
-//   if (!foo.ok) {
-//     console.log('>> E2:', foo.error);
-//     throw foo.error;
-//   }
-
-//   console.log({ result: foo.result });
-// }
-
 function once<A extends Array<unknown>>(
   fn: (...args: A) => void,
 ): (...args: A) => void {
