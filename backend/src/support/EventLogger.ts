15526fb804b819280780181e0edb5926d3efa9c3
Simon <sstur@me.com>
2024-08-06 15:32:48 -0500

    Fixup: Simplify EventLogger

--- backend/src/support/EventLogger.ts
+++ backend/src/support/EventLogger.ts
@@ -15,18 +15,6 @@ export class EventLogger {
     });
   }
 
-  getTimeSinceLast(name: string) {
-    const now = Date.now();
-    const lastEvent = findLast(this.events, (event) => event.name === name);
-    return lastEvent ? now - lastEvent.time : 0;
-  }
-
-  logTimeSince(name: string, description?: string) {
-    const timeElapsed = this.getTimeSinceLast(name);
-    const prefix = description ?? `>> Time since ${name}:`;
-    logger.log(prefix, timeElapsed);
-  }
-
   dumpEventsRelative() {
     let prevEvent: Event | undefined;
     for (const event of this.events) {
@@ -35,15 +23,6 @@ export class EventLogger {
       prevEvent = event;
     }
     this.events.length = 0;
-  }
-}
-
-function findLast<T>(array: Array<T>, fn: (item: T) => boolean): T | undefined {
-  for (let i = array.length - 1; i >= 0; i--) {
-    const item = array[i];
-    if (item && fn(item)) {
-      return item;
-    }
   }
 }
 
