49658ab4ae4501558403a0bd8a4c6ceb1f4c3329
Simon <sstur@me.com>
2024-08-07 10:02:15 -0500

    Try WebView instead

--- mobile/src/features/ConversationalChat.tsx
+++ mobile/src/features/ConversationalChat.tsx
@@ -1,25 +1,19 @@
-import { useState } from 'react';
-import type { WebBrowserResult } from 'expo-web-browser';
-import { openBrowserAsync } from 'expo-web-browser';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { YStack } from 'tamagui';
-
-import { Button, Text } from '../components/core';
+import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import { WebView } from 'react-native-webview';
 
 export function ConversationalChat() {
-  const [result, setResult] = useState<WebBrowserResult | null>(null);
-  const safeAreaInsets = useSafeAreaInsets();
-
-  const handlePressButtonAsync = async () => {
-    const result = await openBrowserAsync(
-      'https://aura-tts-demo.deepgram.com/',
-    );
-    setResult(result);
-  };
   return (
-    <YStack flex={1} ai="center" jc="center" pt={safeAreaInsets.top}>
-      <Button onPress={handlePressButtonAsync}>{t('Open Web Browser')}</Button>
-      <Text>{result && JSON.stringify(result)}</Text>
-    </YStack>
+    <WebView
+      style={styles.container}
+      source={{ uri: 'https://aura-tts-demo.deepgram.com/' }}
+    />
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+  },
+});
