a6151b278daa1734420c0bc37166a71e44a1c8f8
Simon <sstur@me.com>
2024-08-08 16:37:11 -0500

    Revert ConversationalChat.tsx

--- mobile/src/features/ConversationalChat.tsx
+++ mobile/src/features/ConversationalChat.tsx
@@ -1,41 +1,75 @@
+import { useEffect, useReducer, useState } from 'react';
+
 import { Button, Text, VStack } from '../components/core';
 import { useAudioPlayback } from '../context/AudioPlayback';
-import { sleep } from '../support/sleep';
-import { audioClipPcm as audioClipPcmBase64 } from './audioClipPcm';
+import { ConversationController } from '../recording/conversation';
 
-// This must be a multiple of 16 because it will be decoded from base64 (x / 4 * 3) then converted into float32 frames (x / 4).
-const CHUNK_SIZE = 16 * 1024;
+type State =
+  | { name: 'IDLE' }
+  | { name: 'CONVERSATION_ONGOING'; controller: ConversationController };
 
 export function ConversationalChat() {
-  const { playSound } = useAudioPlayback();
+  const audioPlaybackContext = useAudioPlayback();
+  const [state, setState] = useState<State>({ name: 'IDLE' });
+  const [_, forceUpdate] = useReducer((x: number) => x + 1, 0);
+
+  // Subscribe to updates on the conversation controller
+  useEffect(() => {
+    if (state.name === 'CONVERSATION_ONGOING') {
+      const { controller } = state;
+      controller.emitter.on('change', forceUpdate);
+      return () => {
+        controller.emitter.off('change', forceUpdate);
+      };
+    }
+  }, [state, forceUpdate]);
+
+  const isTalking = () => {
+    if (state.name === 'CONVERSATION_ONGOING') {
+      const { controller } = state;
+      if (controller.state.name === 'RUNNING') {
+        const { turn } = controller.state;
+        return turn.name === 'USER_SPEAKING';
+      }
+    }
+    return false;
+  };
 
   return (
     <VStack flex={1} justifyContent="center" alignItems="center">
-      <Text>{t('Ready')}</Text>
-      <Button
-        onPress={() => {
-          const audioStream = getAudioStream(audioClipPcmBase64);
-          void playSound(audioStream, {
-            channels: 1,
-            sampleRate: 16000,
-          });
-        }}
-      >
-        {t('Start')}
-      </Button>
+      {state.name === 'IDLE' ? (
+        <>
+          <Text>{t('Ready')}</Text>
+          <Button
+            onPress={() => {
+              const controller = new ConversationController({
+                audioPlaybackContext,
+              });
+              setState({ name: 'CONVERSATION_ONGOING', controller });
+              void controller.start();
+            }}
+          >
+            {t('Start')}
+          </Button>
+        </>
+      ) : (
+        <>
+          <Text>{t('Running...')}</Text>
+          {isTalking() ? (
+            <Button
+              onPress={() => {
+                const controller = new ConversationController({
+                  audioPlaybackContext,
+                });
+                setState({ name: 'CONVERSATION_ONGOING', controller });
+                void controller.start();
+              }}
+            >
+              {t('Done Talking')}
+            </Button>
+          ) : null}
+        </>
+      )}
     </VStack>
   );
-}
-
-async function* getAudioStream(
-  fullPcmBase64: string,
-): AsyncIterableIterator<string> {
-  const length = fullPcmBase64.length;
-  for (let i = 0; i < length; i += CHUNK_SIZE) {
-    if (i > 0) {
-      await sleep(50);
-    }
-    const chunk = fullPcmBase64.slice(i, i + CHUNK_SIZE);
-    yield chunk;
-  }
 }
