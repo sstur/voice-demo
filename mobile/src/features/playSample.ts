973d864f639e836c999b951485677158fd0a1a7d
Simon <sstur@me.com>
2024-08-08 16:38:24 -0500

    Temp: Add playSample.ts

---
+++ mobile/src/features/playSample.ts
@@ -0,0 +1,38 @@
+import type { AudioPlaybackContext } from '../context/AudioPlayback';
+import { useAudioPlayback } from '../context/AudioPlayback';
+import { sleep } from '../support/sleep';
+import { audioClipPcm as audioClipPcmBase64 } from './audioClipPcm';
+
+// This must be a multiple of 16 because it will be decoded from base64 (x / 4 * 3) then converted into float32 frames (x / 4).
+const CHUNK_SIZE = 16 * 1024;
+
+export async function playSample(audioPlaybackContext: AudioPlaybackContext) {
+  const audioStream = getAudioStream(audioClipPcmBase64);
+  await new Promise<void>((resolve) => {
+    void audioPlaybackContext.playSound(audioStream, {
+      channels: 1,
+      sampleRate: 16000,
+      onDone: () => resolve(),
+    });
+  });
+}
+
+export function usePlaySample() {
+  const audioPlaybackContext = useAudioPlayback();
+  return async () => {
+    await playSample(audioPlaybackContext);
+  };
+}
+
+async function* getAudioStream(
+  fullPcmBase64: string,
+): AsyncIterableIterator<string> {
+  const length = fullPcmBase64.length;
+  for (let i = 0; i < length; i += CHUNK_SIZE) {
+    if (i > 0) {
+      await sleep(50);
+    }
+    const chunk = fullPcmBase64.slice(i, i + CHUNK_SIZE);
+    yield chunk;
+  }
+}
