aaccc0f9c2c3e18e34e6004b7a4cd3347d033484
Simon <sstur@me.com>
2024-08-07 19:58:35 -0500

    Re-apply web-based audioPlayer

--- mobile/src/features/ConversationalChat.tsx
+++ mobile/src/features/ConversationalChat.tsx
@@ -1,69 +1,57 @@
-import { useEffect, useReducer, useState } from 'react';
+import { useRef } from 'react';
+import { WebView } from 'react-native-webview';
 
-import { Button, Text, VStack } from '../components/core';
-import { ConversationController } from '../recording/conversation';
+import { parseMessage } from '../support/parseMessage';
+import { sleep } from '../support/sleep';
+import { audioClipPcm as audioClipPcmBase64 } from './audioClipPcm';
+import { audioPlayerWebViewHtml } from './audioPlayerWebViewHtml';
 
-type State =
-  | { name: 'IDLE' }
-  | { name: 'CONVERSATION_ONGOING'; controller: ConversationController };
+// This must be a multiple of 16 because it will be decoded from base64 (x / 4 * 3) then converted into float32 frames (x / 4).
+const CHUNK_SIZE = 16 * 1024;
 
 export function ConversationalChat() {
-  const [state, setState] = useState<State>({ name: 'IDLE' });
-  const [_, forceUpdate] = useReducer((x: number) => x + 1, 0);
+  const webViewRef = useRef<WebView>(null);
 
-  // Subscribe to updates on the conversation controller
-  useEffect(() => {
-    if (state.name === 'CONVERSATION_ONGOING') {
-      const { controller } = state;
-      controller.emitter.on('change', forceUpdate);
-      return () => {
-        controller.emitter.off('change', forceUpdate);
-      };
-    }
-  }, [state, forceUpdate]);
+  const send = (data: Record<string, unknown>) => {
+    webViewRef.current?.postMessage(JSON.stringify(data));
+  };
 
-  const isTalking = () => {
-    if (state.name === 'CONVERSATION_ONGOING') {
-      const { controller } = state;
-      if (controller.state.name === 'RUNNING') {
-        const { turn } = controller.state;
-        return turn.name === 'USER_SPEAKING';
+  const startStreaming = async () => {
+    const length = audioClipPcmBase64.length;
+    for (let i = 0; i < length; i += CHUNK_SIZE) {
+      if (i > 0) {
+        await sleep(50);
       }
+      const chunk = audioClipPcmBase64.slice(i, i + CHUNK_SIZE);
+      send({ type: 'AUDIO_CHUNK', value: chunk });
     }
-    return false;
+    send({ type: 'AUDIO_DONE' });
   };
 
   return (
-    <VStack flex={1} justifyContent="center" alignItems="center">
-      {state.name === 'IDLE' ? (
-        <>
-          <Text>{t('Ready')}</Text>
-          <Button
-            onPress={() => {
-              const controller = new ConversationController();
-              setState({ name: 'CONVERSATION_ONGOING', controller });
-              void controller.start();
-            }}
-          >
-            {t('Start')}
-          </Button>
-        </>
-      ) : (
-        <>
-          <Text>{t('Running...')}</Text>
-          {isTalking() ? (
-            <Button
-              onPress={() => {
-                const controller = new ConversationController();
-                setState({ name: 'CONVERSATION_ONGOING', controller });
-                void controller.start();
-              }}
-            >
-              {t('Done Talking')}
-            </Button>
-          ) : null}
-        </>
-      )}
-    </VStack>
+    <WebView
+      ref={webViewRef}
+      style={{ flex: 1 }}
+      originWhitelist={['*']}
+      source={{ html: audioPlayerWebViewHtml }}
+      onMessage={(event) => {
+        const data = parseMessage(event.nativeEvent.data);
+        switch (data.type) {
+          case 'READY': {
+            // send({ type: 'LOG', value: 'Hello world' });
+            void startStreaming();
+            break;
+          }
+          case 'LOG': {
+            const { args } = data;
+            if (Array.isArray(args)) {
+              // eslint-disable-next-line no-console
+              console.log(...args);
+            }
+            break;
+          }
+        }
+      }}
+    />
   );
 }
